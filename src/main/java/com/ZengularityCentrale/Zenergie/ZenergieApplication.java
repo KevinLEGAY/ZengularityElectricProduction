c2274ab6e789bb1969b6e9961556c8ac3d6bf187
Kevin legay <kevin.legay@fabernovel.com>
2018-01-18 12:24:26 +0100

    init

---
+++ src/main/java/com/ZengularityCentrale/Zenergie/ZenergieApplication.java
@@ -0,0 +1,12 @@
+package com.ZengularityCentrale.Zenergie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenergieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenergieApplication.class, args);
+	}
+}
